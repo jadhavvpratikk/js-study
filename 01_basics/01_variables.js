58c95eafb3d16dda00f90df62996e6af320f05d4
Pratik Jadhav <134989086+jadhavvpratikk@users.noreply.github.com>
2024-05-25 17:35:00 +0000

    learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "pratikjadhav6396@gmail.com"
+var accountPassword = "123456"
+accountCity = "Mumbai"
+
+//accountId = 789 // not allowed to const
+accountEmail = "pratik@gmail.com"
+
+accountPassword = "111111"
+
+accountCity = "Pune"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])

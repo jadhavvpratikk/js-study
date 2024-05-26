5ced84e761734ec2b82828a2b67d6513effa33d4
Pratik Jadhav <134989086+jadhavvpratikk@users.noreply.github.com>
2024-05-26 04:23:46 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,12 @@ const accountId = 123456
 let accountEmail = "pratikjadhav6396@gmail.com"
 var accountPassword = "123456"
 accountCity = "Mumbai"
+let accountState;
+
+/*
+Prefer not to use var
+because of issue in block and functional scope
+*/
 
 //accountId = 789 // not allowed to const
 accountEmail = "pratik@gmail.com"
@@ -11,4 +17,4 @@ accountPassword = "111111"
 accountCity = "Pune"
 
 console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js codes a newer version
+
+//alert(3+3) //we are using node.js, not browser
+
+console.log(3+3) //code readability should be high
+
+console.log("JS Study")
+
+let name = "pratik"
+let age = 29
+let isAdult = true
+
+/*
+DataTypes - 
+number = 2 power of 53
+bigint = specially in facebook, sharemarket
+string = ""
+boolean = true/false
+null = Standalone value and also datatype
+undefined = it means u declare the variable name and not assigned value to it
+symbol = used to define uniqueness , mostly used in ReactJS
+
+object = 
+
+*/
+
+console.log(typeof "Pratik");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined

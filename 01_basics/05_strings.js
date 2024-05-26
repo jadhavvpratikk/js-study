9a3ffd3feb1474775a3c57beb5fb06e3c432015a
Pratik Jadhav <134989086+jadhavvpratikk@users.noreply.github.com>
2024-05-26 14:00:12 +0000

    String started

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+// let name = "Pratik"
+// const repoCount = 50
+
+//console.log(name + repoCount + " Pushed"); = Not Recommended
+
+//console.log(`Hello my name is ${name.toUpperCase()} and my repoCount is ${repoCount}`);
+//Hello my name is PRATIK and my repoCount is 50
+//console.log(`Hello my name is ${name.toLowerCase()} and my repoCount is ${repoCount}`);
+//Hello my name is pratik and my repoCount is 50
+
+//New way of declaring String 
+const gameName = new String ('GTA Vice-City')
+// console.log(gameName);
+// console.log(gameName[0]);//G
+
+//String methods
+// console.log(gameName.__proto__);//{}
+
+// console.log(gameName.lastIndexOf('e'));//7
+
+// console.log(gameName.length);//13
+
+// console.log(gameName.charAt(4));//V is at 4th index in string gameName
+
+// console.log(gameName.indexOf('T'));//1 is the index of T alphabet in String gameName
+
+// console.log(gameName.toUpperCase());//GTA VICE CITY
+
+// console.log(gameName.toLowerCase());//gta vice city
+
+//Original value of gameName is not changed, it takes a copy of it and changes the value of copy
+
+
+
+

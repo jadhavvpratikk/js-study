f83503f5e56bc00a87ce8267688d5a642796b0e6
Pratik Jadhav <134989086+jadhavvpratikk@users.noreply.github.com>
2024-05-27 09:06:08 +0000

    nums completed

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,26 @@
+// const score = 400
+// console.log(score);//400
+const balance = new Number(100)
+console.log(balance);//[Number: 100]
+console.log(balance.toFixed(2));//100.00 = means 2 decimal fixed value
+// const newString = score.toString();//number is converted to string
+// console.log(newString);//400 - but as a string
+// console.log(typeof newString);//string
+// console.log(newString.length);//3
+
+//Precision is used to roundoff the multiple decimal value to single decimal value
+// const number = 23.8956
+// console.log(number.toPrecision(3));//23.9
+// const number1 = 123.8956
+// console.log(number1.toPrecision(3));//124
+// const number2 = 1123.8956
+// console.log(number2.toPrecision(3));//1.12e+3
+
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString());//100,000 = output in standard format
+// console.log(hundreds.toLocaleString('en-IN'));//1,00,000 = output in Indian standard format
+
+//Maths
+
+

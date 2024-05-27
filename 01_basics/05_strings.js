845d46427251b3332e584a0e8fd50672ff6dcb4e
Pratik Jadhav <134989086+jadhavvpratikk@users.noreply.github.com>
2024-05-27 07:02:56 +0000

    strings complete

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -9,7 +9,7 @@
 //Hello my name is pratik and my repoCount is 50
 
 //New way of declaring String 
-const gameName = new String ('GTA Vice-City')
+//const gameName = new String ('GTA Vice-City')
 // console.log(gameName);
 // console.log(gameName[0]);//G
 
@@ -30,17 +30,33 @@ const gameName = new String ('GTA Vice-City')
 
 //Original value of gameName is not changed, it takes a copy of it and changes the value of copy
 
-const newString = gameName.substring(4,13);//In SubString dont allow negative value it starts from 0th index
+//const newString = gameName.substring(4,13);//In SubString dont allow negative value it starts from 0th index
 //13 because it doesnt include the 13 index of String 
-console.log(newString);//Vice-City
+//console.log(newString);//Vice-City
 
-const sliceString = gameName.slice(0,4);//4 because it doesnt include the 4 index of String
-console.log(sliceString);//GTA
+//const sliceString = gameName.slice(0,4);//4 because it doesnt include the 4 index of String
+//console.log(sliceString);//GTA
 
-const negativeSlice = gameName.slice(-13,-10);
+//const negativeSlice = gameName.slice(-13,-10);
 //In slice, -10 means it goes in reverse direction = -13 means G, -12 means T, -11 means A
 //because it doesnt include the -10 index of String
-console.log(negativeSlice);//GTA
+//console.log(negativeSlice);//GTA
+
+// const stringOne = "   pratik     "
+// console.log(stringOne);//   pratik      - with whitespaces
+// console.log(stringOne.trim());//pratik - with no whitespaces
+
+// const url = "https://pratik.com/pratik%20jadhav"
+// console.log(url.replace("%20","-"));//https://pratik.com/pratik-jadhav - %20 is replaced by -
+
+// console.log(url.includes("pratik"));//true
+
+const splitName = "Pratik-Harichandra-Jadhav"
+console.log(splitName.split("-"));//[ 'Pratik', 'Harichandra', 'Jadhav' ]
+
+
+
+
 
 
 

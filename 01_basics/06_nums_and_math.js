fee5717b214dbc7d68c97211df04667dc9b6ba15
Pratik Jadhav <134989086+jadhavvpratikk@users.noreply.github.com>
2024-06-01 07:15:55 +0000

    Maths file updated

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,8 +1,8 @@
 // const score = 400
 // console.log(score);//400
 const balance = new Number(100)
-console.log(balance);//[Number: 100]
-console.log(balance.toFixed(2));//100.00 = means 2 decimal fixed value
+// console.log(balance);//[Number: 100]
+// console.log(balance.toFixed(2));//100.00 = means 2 decimal fixed value
 // const newString = score.toString();//number is converted to string
 // console.log(newString);//400 - but as a string
 // console.log(typeof newString);//string
@@ -22,5 +22,22 @@ console.log(balance.toFixed(2));//100.00 = means 2 decimal fixed value
 // console.log(hundreds.toLocaleString('en-IN'));//1,00,000 = output in Indian standard format
 
 //Maths
+//console.log(Math);
+// console.log(Math.abs(-4));//convert negative to positive number so output is 4 and positive remains positive only
+// console.log(Math.round(4.3));//rounds to near round number here 4.3 becomes as 4
+// console.log(Math.round(4.7));//rounds to near round number here 4.7 becomes to 5
+//and if user want to control which num is output
+//for highest number we use ceil function
+//console.log(Math.ceil(4.3));//here output is compulsary 5 as upper value is considered
+//for lowest number we use ceil function
+//console.log(Math.floor(4.9));//here output is compulsary 4 as lower value is considered
+
+
+
+
+
+
+
+
 
 
